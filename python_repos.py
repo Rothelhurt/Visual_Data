48d4aec0ba7fab099047975282e26776eaeed2d9
Denis Kolesnik <denis.kolesnik07@gmail.com>
2019-10-23 22:37:04 +0300

    Working with Github.com API/

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LC
 
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -8,5 +10,20 @@ print('Status code:', r.status_code)
 # Сохранение ответа API в переменной.
 response_dict = r.json()
 
-# Обработка результатов.
-print(response_dict.keys())
+print('Total repositories:',  response_dict['total_count'])
+
+# Анализ информации о репозиториях.
+repo_dicts = response_dict['items']
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Построение визуализации.
+my_style =LC('#336699', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Project on Github'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
